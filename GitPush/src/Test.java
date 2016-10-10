3c3c1eae2ab4886ed86f9aedfe16a59e38f77b49
irrationnelle <drakkarverenis@gmail.com>
2016-10-10 20:19:56 +0900

    두번째 커밋

--- GitPush/src/Test.java
+++ GitPush/src/Test.java
@@ -3,5 +3,6 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println
 		("Whatever doesn't kill yourself makes you stranger");
+		System.out.println("The truth is out there");
 	}
 }

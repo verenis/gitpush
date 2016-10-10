28cc6cec79a008a7516cfcb98d083304624e0cb1
verenis <drakkarverenis@hotmail.com>
2016-10-10 20:39:48 +0900

    Update Test.java
    2nd commit

--- GitPush/src/Test.java
+++ GitPush/src/Test.java
@@ -2,6 +2,7 @@
 public class Test {
 	public static void main(String[] args) {
 		System.out.println
-		("Whatever doesn't kill yourself makes you stranger");
+		("Whatever doesn't kill yourself makes you stronger");
+		// by Nietzsche
 	}
 }
